a832981351b44e2fdb48f94cb99553d8e17e1bb4
Thomas <thomassartini@hotmail.com>
2026-05-02 14:41:00 +0200

    fix: shape of AppBottomNav

--- app/src/main/java/ch/hslu/mobpro/dartscore/ui/components/AppBottomNav.kt
+++ app/src/main/java/ch/hslu/mobpro/dartscore/ui/components/AppBottomNav.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -103,8 +104,9 @@ private fun AppBottomNavItem(
             .height(64.dp)
             .background(
                 color = backgroundColor,
-                shape = RoundedCornerShape(16.dp)
+                shape = MaterialTheme.shapes.large
             )
+            .clip(MaterialTheme.shapes.large)
             .clickable { onClick() },
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
